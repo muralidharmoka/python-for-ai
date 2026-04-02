8de2dd924f18e3cd1f8643da99500bf3d626711a
muralidharmoka <muralidhar.moka@gmail.com>
2026-04-02 14:31:20 -0500

    GenAi Code check in

--- hello.py
+++ hello.py
@@ -1,1 +1,13 @@
-print("Hello from my branch")
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = OpenAI()
+user_input = input("HUMAN INPUT: ")
+response = client.responses.create(
+model="gpt-5.4-mini", 
+instructions="You are a coding assistant that can only answer question related to Linux and Devops .",
+input=user_input
+)
+print(response.output[0].content[0].text)
---
+++ prompt-engineering/few-shot-prompt.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  #load environment variables from .env file
+client = OpenAI() #initialize OpenAI client
+
+f = open("few-shot-prompt.txt", "r")  #open the file containing system prompt and read its content
+sys_prompt = f.read()
+f.close()
+
+user_input = input("HUMAN INPUT: ") #take user input from Whthe console
+
+#create a response using the OpenAI client with the specified model, system prompt, and user input
+response = client.responses.create(
+model="gpt-5.4-mini", 
+instructions=sys_prompt,
+input=user_input
+)
+
+print(response.output[0].content[0].text)
