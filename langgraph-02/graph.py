3c75ce40d7d72b48d21b8d999d8c60b4bc72dd22
muralidharmoka <muralidhar.moka@gmail.com>
2026-04-19 17:43:27 -0500

    code added

--- langgraph-02/graph.py
+++ langgraph-02/graph.py
@@ -48,3 +48,30 @@ def developer_agent(state: CodeState):
         "code": result,
         "status": "generated"
     }
+
+# NODE 2: QA AGENT
+def qa_agent(state: CodeState):
+    prompt = f"""
+    You are a sr. strict QA agent for Nodejs.
+    Evaluate the following Nodjs code for the following practices.
+    Evaluate the code based on the following criteria:
+    1. Code correctness: Does the code meet the user's request and function as intended?
+    2. Structure and organization: Is the code well-structured and organized? Are functions and modules used appropriately?
+    3. Production best practices: Does the code follow best practices for production-ready code, such as error handling, security considerations, and performance optimizations?
+    4. error handling: Does the code include proper error handling mechanisms to manage potential issues that may arise during execution?
+    5. use of variables and functions: Are variables and functions used appropriately, with clear naming conventions and without unnecessary complexity?
+    Provide specific feedback on what is wrong with the code if the rating is less than 5.
+  
+    Return a json in the following format:
+    {
+        "rating": integer between (1-10),
+        "feedback": "clear explanation of what is wrong with the code and how to improve it"
+    }
+    Generated code: {state['code']}
+    """
+    result = llm_qa.invoke(prompt).content
+ 
+    return {
+        "rating":int(result['rating']),
+        "feedback": result['feedback'],
+    }
